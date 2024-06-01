a3a2d34043a25c985fb318da53d50e15dbdf3d68
lemon-lime-honey <bs0308@g.skku.edu>
2024-06-02 00:34:34 +0900

    Chapter 04

--- flask-mega-tutorial/app/routes.py
+++ flask-mega-tutorial/app/routes.py
@@ -3,27 +3,23 @@ from app import app
 from app.forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username': 'lime'}
+    user = {"username": "lime"}
     posts = [
-        {
-            'author': {'username': 'berry'},
-            'body': 'Strawberry Fields Forever'
-        },
-        {
-            'author': {'username': 'summer'},
-            'body': 'Summertime Sadness'
-        }
+        {"author": {"username": "berry"}, "body": "Strawberry Fields Forever"},
+        {"author": {"username": "summer"}, "body": "Summertime Sadness"},
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
