b9fd245ca2ea78d1cad030493eeb35fc6e1bff6f
lemon-lime-honey <bs0308@g.skku.edu>
2024-06-05 23:15:57 +0900

    Chapter 10

--- flask-mega-tutorial/app/__init__.py
+++ flask-mega-tutorial/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 from flask_migrate import Migrate
 import logging, os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -12,6 +13,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
+mail = Mail(app)
 
 if not app.debug:
     if app.config["MAIL_SERVER"]:
--- flask-mega-tutorial/app/models.py
+++ flask-mega-tutorial/app/models.py
@@ -6,6 +6,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db, login
+from time import time
+import jwt
+from app import app
 
 
 followers = sa.Table(
@@ -47,6 +50,23 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {"reset_password": self.id, "exp": time() + expires_in},
+            app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])[
+                "reset_password"
+            ]
+        except:
+            return
+        return db.session.get(User, id)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
